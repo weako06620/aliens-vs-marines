36e43637107a8a8ce01f5918aa6a771e3d494ec5
weako06620 <weako06800@gmail.com>
2026-03-08 16:23:20 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'avm-v1';
+const ASSETS = [
+  '/aliens-vs-marines/',
+  '/aliens-vs-marines/index.html',
+  '/aliens-vs-marines/manifest.json',
+  '/aliens-vs-marines/icon-192.png',
+  '/aliens-vs-marines/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
